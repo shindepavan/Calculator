992424523df9a4e52ec57e874a55abe920e86294
shindepavan <shindepavan551@gmail.com>
2019-07-17 20:01:00 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'HomePage.dart';
+void main() => runApp(new MyApp());
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return new MaterialApp(
+      title: "Calculator",
+      theme: new ThemeData(primarySwatch: Colors.red),
+      home: new HomePage(),
+    );
+  }
+}
